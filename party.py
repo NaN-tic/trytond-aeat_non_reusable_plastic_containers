4df51d7f1191fcfc817b1a9d7bb256ae142169ac
Àngel Àlvarez <angel@nan-tic.com>
2022-12-21 12:37:34 +0100

    add party configuration to show details

---
+++ party.py
@@ -0,0 +1,11 @@
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+class Party(metaclass=PoolMeta):
+    __name__ = 'party.party'
+
+    show_plastic_detail = fields.Boolean('Show Plastic Detail')
+
+    @staticmethod
+    def default_show_plastic_detail():
+        return False
