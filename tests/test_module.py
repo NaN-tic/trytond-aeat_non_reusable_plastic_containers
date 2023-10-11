6fd8f2c6c9e9e0f7558033fdebd260b57bbf5c17
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 12:15:33 +0200

    Pyflakes fixes

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,8 +1,6 @@
 # This file is part aeat_non_reusable_plastic_containers module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-import unittest
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
